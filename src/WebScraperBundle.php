00fcc6cdfdff74c6c8675cbafe46d7d57a87ab81
kurozumi <info@a-zumi.net>
2022-10-05 14:24:32 +0900

    fix

--- src/WebScraperBundle.php
+++ src/WebScraperBundle.php
@@ -28,7 +28,7 @@ class WebScraperBundle extends AbstractBundle
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
+        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__ . '/Resources/config'));
         $loader->load('services.yaml');
     }
 }
