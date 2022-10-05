244fc1d8428dea5f8ac46f943ed785a9f17e59a6
kurozumi <info@a-zumi.net>
2022-10-05 14:20:09 +0900

    fix

--- src/WebScraperBundle.php
+++ src/WebScraperBundle.php
@@ -5,10 +5,13 @@ namespace Kurozumi\WebScraperBundle;
 
 use Kurozumi\WebCrawlerBundle\Service\ScraperInterface;
 use Kurozumi\WebScraperBundle\DependencyInjection\Compiler\ScraperPass;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class WebScraperBundle extends Bundle
+class WebScraperBundle extends AbstractBundle
 {
     public function getPath(): string
     {
@@ -21,5 +24,11 @@ class WebScraperBundle extends Bundle
             ->addTag(ScraperPass::TAG);
 
         $container->addCompilerPass(new ScraperPass());
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
+        $loader->load('services.yaml');
     }
 }
